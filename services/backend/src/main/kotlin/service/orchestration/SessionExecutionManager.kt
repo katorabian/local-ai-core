4f880abc63d4333bd25485729dbe2cf25709d886
katorabian <katorabian@gmail.com>
2026-03-10 04:36:09 +0700

    feat(backend): add session ttl

--- services/backend/src/main/kotlin/service/orchestration/SessionExecutionManager.kt
+++ services/backend/src/main/kotlin/service/orchestration/SessionExecutionManager.kt
@@ -8,16 +8,39 @@ import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 /** put requests in stack*/
+
 class SessionExecutionManager {
+
     private val handler = corExcHandler()
-    private val locks = ConcurrentHashMap<UUID, Mutex>()
+    private val locks = ConcurrentHashMap<UUID, SessionLock>()
 
     suspend fun <T> execute(
         executionKey: UUID,
         block: suspend () -> T
     ): T = withContext(handler) {
-        val mutex = locks.computeIfAbsent(executionKey) { Mutex() }
 
-        return@withContext mutex.withLock { block() }
+        val lock = checkNotNull(
+            locks.compute(executionKey) { _, existing ->
+                existing?.updateLastAccess() ?: SessionLock()
+            }
+        )
+        cleanup()
+        return@withContext lock.mutex.withLock { block() }
     }
+
+    private fun cleanup() {
+        val now = System.currentTimeMillis()
+        val ttl = 10 * 60 * 1000L // 10 minutes
+
+        locks.entries.removeIf {
+            now - it.value.lastAccess > ttl
+        }
+    }
+}
+
+private data class SessionLock(
+    val mutex: Mutex = Mutex(),
+    var lastAccess: Long = System.currentTimeMillis()
+) {
+    fun updateLastAccess(): SessionLock = apply { lastAccess = System.currentTimeMillis() }
 }
